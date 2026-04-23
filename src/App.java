faff8a3869a4c494cc0c05892289ceb5276c2bc1
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-04-23 17:48:22 -0300

    Seller entity class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,14 @@
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(1, "Bob Brown", "bobbrown@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
