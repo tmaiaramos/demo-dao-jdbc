400e20a45083507b8d10ca2ff6426aa3aa95c8c5
Tiago Maia Ramos <tramos@mpmg.mp.br>
2026-04-23 18:29:32 -0300

    findById implementation

--- src/App.java
+++ src/App.java
@@ -1,18 +1,16 @@
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Department obj = new Department(1, "Books");
+        //Department obj = new Department(1, "Books");
 
-        Seller seller = new Seller(1, "Bob Brown", "bobbrown@gmail.com", new Date(), 3000.0, obj);
+        //Seller seller = new Seller(1, "Bob Brown", "bobbrown@gmail.com", new Date(), 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         
         System.out.println(seller);
     }
